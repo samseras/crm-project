a3df8f3d007071afe1d288ff68cc16a3025c4286
liumx <liumx@primeton.com>
2019-10-29 14:40:29 +0800

    create new project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <p>cxdd</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
+div{
+  p{
+    font-size: 20px;
+    color: red;
+  }
+}
+</style>
